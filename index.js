29593f8a9244c9f13477f6b4fb7c46e9dc05f1f3
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-06-14 08:10:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 console.log("Starting...");
 
+var CloudmersiveVirusApiClient = require('cloudmersive-virus-api-client');
+var defaultClient = CloudmersiveVirusApiClient.ApiClient.instance;
 
+// Configure API key authorization: Apikey
+var Apikey = defaultClient.authentications['Apikey'];
+Apikey.apiKey = 'YOUR API KEY';
+
+
+
+var apiInstance = new CloudmersiveVirusApiClient.ScanApi();
+
+var inputFile = Buffer.from(fs.readFileSync("C:\\temp\\output.csv").buffer); // File | Input file to perform the operation on.
+
+
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+apiInstance.scanFile(inputFile, callback);
 
 console.log("Done.");
